966c5ff516a0ce1d51124106635250aa277d4b55
krismile-sunflower <892212481@qq.com>
2021-01-27 14:05:31 +0800

    新增登录、注册页面

--- a/src/components/Login.vue
+++ a/src/components/Login.vue
@@ -1,75 +1,100 @@
 <template>
-  <div>
-    <div id="enter">
-      <h1>学生管理系统</h1>
-      <el-input v-model="username" placeholder="请输入账号"></el-input>
-      <br /><br />
-      <el-input
-        placeholder="请输入密码"
-        v-model="password"
-        show-password
-      ></el-input>
-      <br /><br />
-      <el-button type="primary" style="margin-left: 120px" @click="register()"
-        >注册</el-button
-      >
-      <el-button type="success" @click="login()">登录</el-button>
-    </div>
-  </div>
+<el-container>
+    <el-header style="height: 184px"></el-header>
+    <el-main>
+        <div class="login">
+            <el-form :label-position="labelPosition" label-width="80px">
+                <h1>学生管理系统</h1>
+                <el-form-item label="账号">
+                    <el-input v-model="username" placeholder="请输入账号"></el-input>
+                </el-form-item>
+                <el-form-item label="密码">
+                    <el-input placeholder="请输入密码" v-model="password" show-password></el-input>
+                </el-form-item>
+                <el-button @click="rememberPassword()" style="height: 14px; padding: 0; border: none">
+                    <el-checkbox v-model="checked" label-position="left"></el-checkbox>
+                    记住密码
+                </el-button>
+                <br><br>
+                <el-button type="primary" @click="register()">注册</el-button>
+                <el-button type="success" @click="login()">登录</el-button>
+            </el-form>
+        </div>
+    </el-main>
+    <el-footer style="height: 190px"></el-footer>
+</el-container>
 </template>
 
-<style>
-body {
-  background-color: aqua;
-  background-repeat: no-repeat;
-  background-size: 100%;
-}
-#enter {
-  height: 200px;
-  width: 280px;
-  margin: 250px auto;
+<style lang="scss" scoped>
+.el-container {
+    width: 100%;
+    height: 100%;
+    background: url("../assets/log.jpg");
+    background-repeat: no-repeat;
+    background-size: 100%;
+
+    .el-main {
+        margin: 0 35%;
+        text-align: cneter;
+
+        .login {
+            text-align: center;
+            width: 400px;
+            padding: 10px;
+            background-color: white;
+
+        }
+    }
 }
 </style>
 
 <script>
 export default {
-  data() {
-    return {
-      username: "",
-      password: "",
-      dialogFormVisible: false,
-    };
-  },
-  methods: {
-    login: function() {
-      this.$axios
-        .get("http://localhost:8090/user/login", {
-          params: { username: this.username, password: this.password }
-        })
-        .then(response => {
-          console.log(response.data);
-          console.log(this.username);
-          if (response.data === "ok") {
-            localStorage.setItem("username", this.username);
-            localStorage.setItem("password", this.password);
-            localStorage.setItem("status", "ok");
-            this.$message("登陆成功");
-            window.location.href = "/student";
-          } else if (response.data === "error username") {
-            this.$message("用户名错误");
-          } else if (response.data === "error password") {
-            this.$message("密码错误");
-          }
-        })
-        .catch(function(error) {
-          // 请求失败处理
-          console.log(error);
-        });
+    data() {
+        return {
+            labelPosition: 'right',
+            username: "",
+            password: "",
+            dialogFormVisible: false,
+            checked: false
+        };
     },
-    register: function() {
-      this.$router.push({path:'/register'})
+    methods: {
+        login: function () {
+            this.$axios
+                .get("http://localhost:8090/user/login", {
+                    params: { username: this.username, password: this.password }
+                })
+                .then(response => {
+                    console.log(response.data);
+                    console.log(this.username);
+                    if (response.data === "ok") {
+                        localStorage.setItem("username", this.username);
+                        localStorage.setItem("password", this.password);
+                        localStorage.setItem("status", "ok");
+                        this.$message("登陆成功");
+                        window.location.href = "/student";
+                    } else if (response.data === "error username") {
+                        this.$message("用户名错误");
+                    } else if (response.data === "error password") {
+                        this.$message("密码错误");
+                    }
+                })
+                .catch(function (error) {
+                    // 请求失败处理
+                    console.log(error);
+                });
+        },
+        register: function () {
+            this.$router.push({ path: '/register' })
+        },
+        rememberPassword: function () {
+            this.checked = !this.checked;
+            if (this.checked == true){
+              sessionStorage.setItem("username", this.username);
+              sessionStorage.setItem("password", this.password);
+            }
+        }
     }
-  }
 };
 </script>
-
--- a/src/components/Register.vue
+++ a/src/components/Register.vue
@@ -1,102 +1,113 @@
 <template>
-  <div id="center">
-    <el-form :model="form" :rules="rules">
-      <el-form-item label="账号" :label-width="formLabelWidth" prop="username">
-        <el-input v-model="form.username" autocomplete="off"></el-input>
-      </el-form-item>
-      <el-form-item label="密码" :label-width="formLabelWidth" prop="password">
-        <el-input
-          v-model="form.password"
-          autocomplete="off"
-          show-password
-        ></el-input>
-      </el-form-item>
-    </el-form>
-    <el-form :model="checkForm">
-      <el-form-item label="确认密码" :label-width="formLabelWidth">
-        <el-input
-          v-model="checkForm.checkPassword"
-          autocomplete="off"
-          show-password
-        ></el-input>
-      </el-form-item>
-    </el-form>
-    <div slot="footer" class="dialog-footer">
-      <el-button @click="cancle()">取 消</el-button>
-      <el-button type="primary" @click="register()">确 定</el-button>
+<div id="center">
+    <div class="register">
+        <h1>注册</h1>
+        <el-form :model="form" :rules="rules">
+            <el-form-item label="账号" :label-width="formLabelWidth" prop="username">
+                <el-input v-model="form.username" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" :label-width="formLabelWidth" prop="password">
+                <el-input v-model="form.password" autocomplete="off" show-password></el-input>
+            </el-form-item>
+        </el-form>
+        <el-form :model="checkForm">
+            <el-form-item label="确认密码" :label-width="formLabelWidth">
+                <el-input v-model="checkForm.checkPassword" autocomplete="off" show-password></el-input>
+            </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="cancle()">取 消</el-button>
+            <el-button type="primary" @click="register()">确 定</el-button>
+        </div>
     </div>
-  </div>
+</div>
 </template>
 
-<style>
+<style lang="less" scoped>
 #center {
-  width: 500px;
-  margin: 0 auto;
-  margin-top: 200px;
-}
-.dialog-footer {
-  padding-left: 320px;
+    width: 100%;
+    height: 100%;
+    background: url("../assets/log.jpg");
+    background-repeat: no-repeat;
+    background-size: 100%;
+    padding: 188px 0;
+
+    h1 {
+      text-align: center;
+    }
+
+    .register {
+        background-color: white;
+        width: 400px;
+        margin: 0 35%;
+        padding: 10px;
+      
+        .dialog-footer {
+            text-align: center;
+        }
+    }
+
 }
 </style>
 
 <script>
 export default {
-  data() {
-    return {
-      form: {
-        username: "",
-        password: ""
-      },
-      checkForm: {
-        checkPassword: ""
-      },
-      formLabelWidth: "80px",
-      rules: {
-          username: [
-            { required: true, message: '请输入你的用户名', trigger: 'blur' },
-          ],
-          password: [
-             { required: true, message: '请输入你的密码', trigger: 'blur' },
-             { min: 5, message: '长度至少为 5 个字符', trigger: 'blur' }
-          ]
-        }
-    };
-  },
-  methods: {
-    cancle: function() {
-      this.$router.push({ path: "/" });
+    data() {
+        return {
+            form: {
+                username: "",
+                password: ""
+            },
+            checkForm: {
+                checkPassword: ""
+            },
+            formLabelWidth: "80px",
+            rules: {
+                username: [
+                    { required: true, message: '请输入你的用户名', trigger: 'blur' },
+                ],
+                password: [
+                    { required: true, message: '请输入你的密码', trigger: 'blur' },
+                    { min: 5, message: '长度至少为 5 个字符', trigger: 'blur' }
+                ]
+            }
+        };
     },
-    register: function() {
-      console.log(this.form.username);
-      console.log(this.form.password);
-      console.log(this.checkForm.checkPassword);
-      if (this.form.password != this.checkForm.checkPassword) {
-        this.$message({ message: "输入的密码不一致", type: "error" });
-        return;
-      }
-      this.$axios
-        .post("http://localhost:8090/user/add", this.form)
-        .then(response => {
-          console.log(response.data);
-          if (response.data == 2) {
-            this.$message({ message: "该用户名已经被注册", type: "error" });
-          } else if (response.data == 0) {
-            this.$message({ message: "该用户名不能为空", type: "error" });
-          } else if (response.data == 1) {
-            this.$alert("注册成功", {
-              confirmButtonText: "确定",
-              callback: action => {
-                console.log(action);
-                this.$message({
-                  type: "success",
-                  message: "现在你已经注册了账号"
+    methods: {
+        cancle: function () {
+            this.$router.push({ path: "/" });
+        },
+        register: function () {
+            console.log(this.form.username);
+            console.log(this.form.password);
+            console.log(this.checkForm.checkPassword);
+            if (this.form.password != this.checkForm.checkPassword) {
+                this.$message({ message: "输入的密码不一致", type: "error" });
+                return;
+            }
+            this.$axios
+                .post("http://localhost:8090/user/add", this.form)
+                .then(response => {
+                    console.log(response.data);
+                    if (response.data == 2) {
+                        this.$message({ message: "该用户名已经被注册", type: "error" });
+                    } else if (response.data == 0) {
+                        this.$message({ message: "该用户名不能为空", type: "error" });
+                    } else if (response.data == 1) {
+                        this.$alert("注册成功", {
+                            confirmButtonText: "确定",
+                            callback: action => {
+                                console.log(action);
+                                this.$message({
+                                    type: "success",
+                                    message: "现在你已经注册了账号"
+                                });
+                                this.$router.push({ path: "/" });
+                            }
+                        });
+                    }
                 });
-                this.$router.push({ path: "/" });
-              }
-            });
-          }
-        });
+        }
     }
-  }
 };
 </script>
